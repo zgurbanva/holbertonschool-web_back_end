9d44eec1e00ac4dbdf573820afa93c74caeba886
Zeynab <zeyneb.gurbanva@gmail.com>
2025-11-28 17:01:58 +0000

    commit

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,4 +7,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """Collect 10 random numbers from async_generator using async comprehension."""
-    return [x async for x in async_generator()]
+    result = [item async for item in async_generator()]
+    return result
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Measure the runtime of executing async_comprehension four times in parallel."""
+"""Measure the runtime of async_comprehension executed 4 times."""
 import asyncio
 import time
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__(
+    '1-async_comprehension'
+).async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Run async_comprehension 4 times concurrently and measure total execution time."""
+    """Run async_comprehension 4 times concurrently and return total runtime."""
     start = time.perf_counter()
 
     await asyncio.gather(
