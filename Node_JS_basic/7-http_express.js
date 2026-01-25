8d3847e2be2f96737c13f224a44c479c4998fd6f
zgurbanva <zeyneb.gurbanva@gmail.com>
2026-01-25 22:47:40 +0400

    commit

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,52 +1,31 @@
+// small HTTP server using Express module
+// same as 6-http_server.js with the addition of the following:
+// '/' => 'Hello Holberton School!'
+// '/students/ => 'This is the list of our students' + list of students
+
 const express = require('express');
-const fs = require('fs');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const database = process.argv[2];
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-
-  fs.readFile(database, 'utf8', (err, data) => {
-    if (err) {
-      res.end('Cannot load the database');
-      return;
-    }
-
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter((line) => line.trim() !== '');
-
-    res.write(`Number of students: ${students.length}\n`);
-
-    const fields = {};
-
-    students.forEach((line) => {
-      const parts = line.split(',');
-      const firstname = parts[0];
-      const field = parts[3];
-
-      if (!fields[field]) {
-        fields[field] = [];
-      }
-      fields[field].push(firstname);
+app.get('/students', async (req, res) => {
+  await countStudents(process.argv[2])
+    .then((data) => {
+      res.write('This is the list of our students\n');
+      res.write(`Number of students: ${data.CS.num + data.SWE.num}\n`);
+      res.write(`Number of students in CS: ${data.CS.num}. List: ${data.CS.list}\n`);
+      res.write(`Number of students in SWE: ${data.SWE.num}. List: ${data.SWE.list}`);
+      res.end();
+    })
+    .catch((err) => {
+      res.write('This is the list of our students\n');
+      res.end(err.message);
     });
-
-    Object.keys(fields).forEach((field) => {
-      res.write(
-        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`
-      );
-    });
-
-    res.end();
-  });
-});
-
-app.listen(1245);
+}).listen(port);
 
 module.exports = app;
-
